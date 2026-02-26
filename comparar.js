49e723e6fea629b17e3315a0f4ed2f2d7346a926
Gabriel <gabrielalveslga1155@gmail.com>
2026-02-26 14:42:58 -0300

    fiz o comparar.html

---
+++ comparar.js
@@ -0,0 +1,30 @@
+// Pegando todos os simbolos pra fazer a comparação...
+
+
+//<==============          =======================>
+
+const  copilot = document.getElementById('copilot')
+
+const chatgpt = document.getElementById('chatgpt')
+
+const gemini = document.getElementById('gemini')
+
+const meta = document.getElementById('meta')
+
+const ul = document.querySelector('ul.IA')
+
+//<==============          =======================>
+
+copilot.addEventListener('click', () => {
+    let cop = document.createElement('img')
+    copilot.src = '../imagens/copilot-logo.png';
+    ul.appendChild(copilot)
+
+
+
+})
+
+chatgpt.addEventListener('click', () => {
+    let chat = document.createElement('img')
+    chat.str
+})
